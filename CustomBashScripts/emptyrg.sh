5bdf0c8640099b6d8979a9b1b6abdb2ed591ff64
Chad Crowell <chad.crowell@linuxacademy.com>
2020-03-08 14:17:58 -0500

    added empty rg

---
+++ CustomBashScripts/emptyrg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# install powershell 
+sudo snap install powershell --classic
+
+sleep 30
+
+pwsh
+
+New-AzSubscriptionDeployment `
+  -Name demoDeployment `
+  -Location westus `
+  -TemplateUri https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/azure-resource-manager/emptyRG.json `
+  -rgName demoResourceGroup `
+  -rgLocation westus
