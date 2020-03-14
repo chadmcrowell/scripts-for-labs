680e5d9ffbd77d81979327ce1a9a5c3a94ab16ab
Chad Crowell <chad.crowell@linuxacademy.com>
2020-03-14 13:41:10 -0500

    changes to vscode on win pwsh script

--- CustomPowerShellScripts/vscode-on-windows.ps1
+++ CustomPowerShellScripts/vscode-on-windows.ps1
@@ -4,4 +4,8 @@ choco install -y vscode
 
 choco install -y git
 
+choco install -y kubernetes-cli
+
+choco install -y kubernetes-helm
+
 Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
