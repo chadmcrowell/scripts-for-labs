af4712cdd2b287b3aba585bf6816e56e7d6e7f0c
Chad Crowell <chad.crowell@linuxacademy.com>
2020-03-14 12:55:15 -0500

    changes to vscode on windows ps script

--- CustomPowerShellScripts/vscode-on-windows.ps1
+++ CustomPowerShellScripts/vscode-on-windows.ps1
@@ -1,3 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install -y vscode
+
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
