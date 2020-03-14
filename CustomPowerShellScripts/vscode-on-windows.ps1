41661956ef2a5e693edb0e85c278559f3b590747
Chad Crowell <chad.crowell@linuxacademy.com>
2020-03-14 14:20:29 -0500

    changes to win vscode vm pwsh script

--- CustomPowerShellScripts/vscode-on-windows.ps1
+++ CustomPowerShellScripts/vscode-on-windows.ps1
@@ -10,4 +10,4 @@ choco install -y kubernetes-helm
 
 Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
 
-Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/5aad9c2c-7bb6-45b1-97e7-98f12cb5b63b/6f6d7944c81b043bdb9a7241529a5504/dotnet-sdk-3.1.102-win-x64.exe -Outfile C:\Users\Public\Downloads\pwsh.msi
+Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/5aad9c2c-7bb6-45b1-97e7-98f12cb5b63b/6f6d7944c81b043bdb9a7241529a5504/dotnet-sdk-3.1.102-win-x64.exe -Outfile C:\Users\Public\Downloads\dotnet-sdk-3.1.102-win-x64.exe
