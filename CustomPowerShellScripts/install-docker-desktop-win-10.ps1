f84f4f49499cb189cdce2fc24e45cec1adc3db31
Chad Crowell <chad.crowell@linuxacademy.com>
2020-04-08 08:32:12 -0500

    changed docker win vm

--- CustomPowerShellScripts/install-docker-desktop-win-10.ps1
+++ CustomPowerShellScripts/install-docker-desktop-win-10.ps1
@@ -1,6 +1,2 @@
 Invoke-WebRequest -Uri https://download.docker.com/win/stable/Docker%20Desktop%20Installer.exe -OutFile C:\Users\Public\Downloads\DockerDesktopInstaller.exe
 
-Start-Sleep -s 30
-
-Start-Process -FilePath "C:\Users\Public\Downloads\DockerDesktopInstaller.exe"
-
