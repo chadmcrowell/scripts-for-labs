d3f9a893c056d2c3b7170c6118eb09bc7d3661a8
Chad Crowell <chad.crowell@linuxacademy.com>
2020-07-21 13:23:05 -0500

    Update git-clone.ps1

--- PowerShell/git-clone.ps1
+++ PowerShell/git-clone.ps1
@@ -1,1 +1,5 @@
 git clone https://github.com/chadmcrowell/fileshare.git c:\FileShare1
+
+New-SmbShare -Name "Code" -Path "C:\code"
+
+New-PSDrive -Name "S" -Root "\\lab-VM\code" -Persist -PSProvider "FileSystem"
